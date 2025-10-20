b7cec26a79473b7ac37ed20fafe284b695c66a90
yangyuwen <yuwen.yang@doforsys.com>
2025-10-20 10:39:17 +0800

    feat: cli 脚本

---
+++ packages/pe-lint/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['encode-fe-eslint-config/typescript/node', 'prettier'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 0,
+      'no-console': 0,
+    },
+  };
+  
---
+++ packages/pe-lint/src/actions/init.ts
@@ -0,0 +1,156 @@
+import path from "path";
+import fs from "fs-extra";
+import inquirer from "inquirer";
+import spawn from "cross-spawn";
+import update from "./update";
+import npmType from "../utils/npm-type";
+import log from "../utils/log";
+import conflictResolve from '../utils/conflict-resolve';
+import generateTemplate from '../utils/generate-template';
+import { InitOptions, PKG } from "../types";
+import { PKG_NAME, PROJECT_TYPES } from "../utils/constants";
+
+let step = 0;
+
+const chooseEslintType = async (): Promise<string> => {
+    const { type } = await inquirer.prompt({
+        type: 'list',
+        name: 'type',
+        message: `Step ${++step}. 请选择项目的语言（JS/TS）和框架（React/Vue）类型：`,
+        choices: PROJECT_TYPES
+    })  
+
+    return type;
+}
+
+const chooseEnableStylelint = async (defaultValue: boolean): Promise<boolean> => {
+    const { enable } = await inquirer.prompt({
+        type: 'confirm',
+        name: 'enable',
+        message: `Step ${++step}. 是否需要使用 stylelint（若没有样式文件则不需要）：`,
+        default: defaultValue
+    })
+
+    return enable;
+}
+
+const chooseEnableMarkdownLint = async (): Promise<boolean> => {
+    const { enable } = await inquirer.prompt({
+        type: 'confirm',
+        name: 'enable',
+        message: `Step ${++step}. 是否需要使用 markdownlint（若没有 Markdown 文件则不需要）：`,
+        default: true,
+    });
+
+    return enable;
+}
+
+const chooseEnablePrettier = async (): Promise<boolean> => {
+    const { enable } = await inquirer.prompt({
+        type: 'confirm',
+        name: 'enable',
+        message: `Step ${++step}. 是否需要使用 Prettier 格式化代码：`,
+        default: true,
+      });
+    
+      return enable;
+}
+
+export default async (options: InitOptions) => {
+    const cwd = options.cwd || process.cwd();
+    const isTest = process.env.NODE_ENV === 'test';
+    const checkVersionUpdate = options.checkVersionUpdate || false;
+    const disabledNpmInstall = options.disableNpmInstall || false;
+    const config: Record<string, any> = {};
+    const pkgPath = path.resolve(cwd, 'package.json');
+    let pkg: PKG = fs.readJSONSync(pkgPath);
+
+    // 版本检查
+    if(!isTest && checkVersionUpdate) {
+        await update(false);
+    }
+
+    // 初始化 `enableESLint`, 默认为 true 无需让用户选择
+    if (typeof options.enableESLint === 'boolean') {
+        config.enableESLint = options.enableESLint;
+    } else {
+        config.enableESLint = true;
+    }
+
+    // 初始化 `eslintType`
+    if (options.eslintType && PROJECT_TYPES.find((choice) => choice.value === options.eslintType)) {
+        config.eslintType = options.eslintType
+    } else {
+        config.eslintType = await chooseEslintType()
+    }
+
+    // 初始化 `enableStylelint`
+    if (typeof options.enableStylelint === 'boolean') {
+        options.enableStylelint = options.enableStylelint
+    } else {
+        options.enableStylelint = await chooseEnableStylelint(!/node/.test(config.eslintType))
+    }
+
+    // 初始化 `enableMarkdownlint`
+    if (typeof options.enableMarkdownlint === 'boolean') {
+        options.enableMarkdownlint = options.enableMarkdownlint
+    } else {
+        options.enableMarkdownlint = await chooseEnableMarkdownLint()
+    }
+
+    // 初始化 `enablePrettier`
+    if (typeof options.enablePrettier === 'boolean') {
+        options.enablePrettier = options.enableMarkdownlint;
+    } else {
+        options.enablePrettier = await chooseEnablePrettier()
+    }
+
+    if (!isTest) {
+        log.info(`Step ${++step}. 检查并处理项目中可能存在的依赖和配置冲突`);
+        pkg = await conflictResolve(cwd, options.rewriteConfig);
+        log.success(`Step ${step}. 已完成项目依赖和配置冲突检查处理 :D`);
+
+        if (!disabledNpmInstall) {
+            log.info(`Step ${++step}. 安装依赖`);
+            const npm = await npmType;
+            spawn.sync(
+                npm,
+                ['i', '-D', PKG_NAME],
+                { stdio: 'inherit', cwd }
+            );
+            log.success(`Step ${step}. 安装依赖成功 :D`);
+        }
+    }
+    // 更新pkg.json
+    pkg = fs.readJSONSync(pkgPath);
+    // 在package.json 中写入 script
+    if (!pkg.scripts) {
+        pkg.scripts = {}
+    }
+
+    if (!pkg.scripts[`${PKG_NAME}-scan`]) {
+        pkg.scripts[`${PKG_NAME}-scan`] = `${PKG_NAME} scan`
+    }
+
+    if (!pkg.scripts[`${PKG_NAME}-fix`]) {
+        pkg.scripts[`${PKG_NAME}-fix`]  =`${PKG_NAME} fix`
+    }
+
+    // 配置 commit 卡点
+    log.info(`Step ${++step}. 配置 git commit 卡点`);
+    if (!pkg.husky) pkg.husky = {};
+    if (!pkg.husky.hooks) pkg.husky.hooks = {};
+    pkg.husky.hooks['pre-commit'] = `${PKG_NAME} commit-file-scan`;
+    pkg.husky.hooks['commit-msg'] = `${PKG_NAME} commit-msg-scan`;
+
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+    log.success(`Step ${step}. 配置 git commit 卡点成功 :D`);
+
+    log.info(`Step ${++step}. 写入配置文件`);
+    generateTemplate(cwd, config);
+    log.success(`Step ${step}. 写入配置文件成功 :D`);
+
+    // 完成信息
+    const logs = [`${PKG_NAME} 初始化完成 :D`].join('\r\n');
+    log.success(logs)
+}
