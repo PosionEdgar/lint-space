0558a4958fa4b7d58aa5fdbd1f2bf823f5a12fea
yangyuwen <yuwen.yang@doforsys.com>
2025-10-21 13:52:41 +0800

    fix: 错误修复

--- packages/pe-lint/src/actions/scan.ts
+++ packages/pe-lint/src/actions/scan.ts
@@ -1,66 +1,66 @@
 import fs from 'fs-extra';
 import path from 'path';
-import { doEslint, doPrettier, doStylelint, doMarkdownlint } from '../lints'
-
-import type { Config, PKG, ScanOptions, ScanReport, ScanResult } from "../types";
+import { doESLint, doMarkdownlint, doPrettier, doStylelint } from '../lints';
+import type { Config, PKG, ScanOptions, ScanReport, ScanResult } from '../types';
 import { PKG_NAME } from '../utils/constants';
 
 export default async (options: ScanOptions): Promise<ScanReport> => {
-    const { cwd, fix, outputReport, config: scanConfig } = options;
+  const { cwd, fix, outputReport, config: scanConfig } = options;
 
-    const readConfigFile = (pth: string): any => {
-        const localPath = path.resolve(cwd, pth);
-        return fs.existsSync(localPath) ? require(localPath) : {}
-    }
+  const readConfigFile = (pth: string): any => {
+    const localPath = path.resolve(cwd, pth);
+    return fs.existsSync(localPath) ? require(localPath) : {};
+  };
+  const pkg: PKG = readConfigFile('package.json');
+  const config: Config = scanConfig || readConfigFile(`${PKG_NAME}.config.js`);
+  const runErrors: Error[] = [];
+  let results: ScanResult[] = [];
 
-    const pkg: PKG = readConfigFile('package.json');
-    const config: Config = scanConfig || readConfigFile(`${PKG_NAME}.config.js`);
-    const runErrors: Error[] = [];
-    let results: ScanResult[] = [];
+  // prettier
+  if (fix && config.enablePrettier !== false) {
+    await doPrettier(options);
+  }
 
+  // eslint
+  if (config.enableESLint !== false) {
+    try {
+      const eslintResults = await doESLint({ ...options, pkg, config });
+      results = results.concat(eslintResults);
+    } catch (e) {
+      runErrors.push(e);
+    }
+  }
 
-    // prettier
-    if (fix && config.enablePrettier) {
-        await doPrettier(options)
+  // stylelint
+  if (config.enableStylelint !== false) {
+    try {
+      const stylelintResults = await doStylelint({ ...options, pkg, config });
+      results = results.concat(stylelintResults);
+    } catch (e) {
+      runErrors.push(e);
     }
-    // eslint
-    if (!config.enableESLint) {
-        try {
-            const eslintResults = await doEslint({ ...options, pkg, config })
-            results = results.concat(eslintResults)
-        } catch (e) {
-            runErrors.push(e)
-        }
-    }
-    // stylelint
-    if (!config.enableStylelint) {
-        try {
-            const stylelintResult = await doStylelint({ ...options, pkg, config })
-            results.concat(stylelintResult)
-        } catch (e) {
-            runErrors.push(e)
-        }
-    }
-    // markdownlint
-    if (!config.enableMarkdownlint) {
-        try {
-            const markdownlintResult = await doMarkdownlint({ ...options, pkg, config })
-            results.concat(markdownlintResult)
-        } catch (e) {
-            runErrors.push(e)
-        }
-    }
+  }
 
-    // 生成测试报告
-    if (outputReport) {
-        const reportPath = path.resolve(process.cwd(), `./${PKG_NAME}-report.json`)
-        fs.outputFile(reportPath, JSON.stringify(results, null, 2), () => {})
+  // markdown
+  if (config.enableMarkdownlint !== false) {
+    try {
+      const markdownlintResults = await doMarkdownlint({ ...options, pkg, config });
+      results = results.concat(markdownlintResults);
+    } catch (e) {
+      runErrors.push(e);
     }
+  }
 
-    return {
-        results,
-        errorCount: results.reduce((count, { errorCount }) => count + errorCount, 0),
-        warningCount: results.reduce((count, { warningCount }) => count + warningCount, 0),
-        runErrors
-    }
-}
+  // 生成报告报告文件
+  if (outputReport) {
+    const reportPath = path.resolve(process.cwd(), `./${PKG_NAME}-report.json`);
+    fs.outputFile(reportPath, JSON.stringify(results, null, 2), () => {});
+  }
+
+  return {
+    results,
+    errorCount: results.reduce((count, { errorCount }) => count + errorCount, 0),
+    warningCount: results.reduce((count, { warningCount }) => count + warningCount, 0),
+    runErrors,
+  };
+};
