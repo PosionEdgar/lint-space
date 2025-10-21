7cea407566fdf9ea57a57bf38389d23095697e9c
yangyuwen <yuwen.yang@doforsys.com>
2025-10-21 20:53:52 +0800

    fix: 错误修正

--- packages/web-pe-lint/src/lints/eslint/doEslint.ts
+++ packages/web-pe-lint/src/lints/eslint/doEslint.ts
@@ -1,4 +1,3 @@
-import { ESLint } from 'eslint';
 import fg from 'fast-glob';
 import { extname, join } from 'path';
 import { Config, PKG, ScanOptions } from '../../types';
@@ -6,12 +5,20 @@ import { ESLINT_FILE_EXT, ESLINT_IGNORE_PATTERN } from '../../utils/constants';
 import { formatESLintResults } from './formatESLintResults';
 import { getESLintConfig } from './getESLintConfig';
 
+
+// 👇 强制从当前包解析 ESLint，防止引用到错误版本
+const { ESLint } = require(require.resolve('eslint', { paths: [__dirname] }));
 export interface DoESLintOptions extends ScanOptions {
   pkg: PKG;
   config?: Config;
 }
 
 export async function doESLint(options: DoESLintOptions) {
+  console.log('[DEBUG] eslint module path:', require.resolve('eslint', { paths: [__dirname] }));
+  console.log('[DEBUG] eslint exports keys:', Object.keys(require('eslint')));
+  console.log('[DEBUG] eslint version:', require('eslint/package.json').version);
+  
+  
   let files: string[];
   if (options.files) {
     files = options.files.filter((name) => ESLINT_FILE_EXT.includes(extname(name)));
