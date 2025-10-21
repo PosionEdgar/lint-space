2322ad05d20952108ee6b699a08c9c27225b29cf
yangyuwen <yuwen.yang@doforsys.com>
2025-10-21 14:04:08 +0800

    fix: 错误修复

--- packages/pe-lint/src/utils/npm-type.ts
+++ packages/pe-lint/src/utils/npm-type.ts
@@ -1,13 +1,12 @@
-import { sync as commandExistsSync } from "command-exists";
+import { sync as commandExistsSync } from 'command-exists';
 
 /**
- * npm 类型 
- * */ 
-
+ * npm 类型
+ */
 const promise: Promise<'npm' | 'pnpm'> = new Promise((resolve) => {
-    if (!commandExistsSync('pnpm')) return resolve('npm')
-    
-    resolve('pnpm')
-})
+  if (!commandExistsSync('pnpm')) return resolve('npm');
+
+  resolve('pnpm');
+});
 
 export default promise;
